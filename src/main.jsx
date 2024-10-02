0ebec109ff7be1a7aa7f44cdb10aaf46aba87c3f
Gerardo <147111986+gerimosca@users.noreply.github.com>
2024-10-02 09:33:13 +0200

    update template

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('root');
+
+createRoot(container).render(<App />); // Al final de App hacer CTRL+SPACE para importarlo (para que se pueda importar tiene que estar abierto como pestaña el archivo App.jsx)
